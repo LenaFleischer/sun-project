7d30045b96ff386c6295a354bd9195ac8bbda9ec
ellenmoore <em_moore@coloradocollege.edu>
2023-01-30 15:49:13 -0700

    good,bad,fine edits

--- sun_test/sun_test/ContentView.swift
+++ sun_test/sun_test/ContentView.swift
@@ -7,10 +7,48 @@
 
 import SwiftUI
 
+let backgroundGood = LinearGradient(
+    colors: [Color.pink, Color.orange],
+    startPoint: .top, endPoint: .bottom)
+let backgroundBad = LinearGradient(
+    colors: [Color.black, Color.blue],
+    startPoint: .top, endPoint: .bottom)
+let backgroundFine = LinearGradient(
+    colors: [Color.blue, Color.yellow],
+    startPoint: .top, endPoint: .bottom)
+
 struct ContentView: View {
     var body: some View {
-        Text("Sunrise is good!")
-            .padding()
+        
+        let condition = "fine";
+        if (condition == "good"){
+            ZStack{
+                backgroundGood
+                VStack{
+                    Text("Sunrise is good!")
+                        .padding()
+                }
+            }
+        }
+        else if (condition == "bad"){
+            ZStack{
+                backgroundBad
+                VStack{
+                    Text("Sunrise is bad!")
+                        .padding()
+                }
+            }
+        }
+        else {
+            ZStack{
+                backgroundFine
+                VStack{
+                    Text("Sunrise is fine!")
+                        .padding()
+                }
+            }
+        }
+        
     }
 }
 
