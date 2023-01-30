c03d49fecc5c212c8c447938db916aa38d967cb3
LenaFleischer <>
2023-01-30 14:24:38 -0700

    hello world

---
+++ sun_test/sun_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sun_test
+//
+//  Created by Lena Fleischer on 1/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Sunrise is good!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sun_test/sun_test/sun_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  sun_testApp.swift
+//  sun_test
+//
+//  Created by Lena Fleischer on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct sun_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
