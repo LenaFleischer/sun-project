850007d8a6e3cb8cd7ba71330e77dd8557a09f3a
ellen-moore <99365584+ellen-moore@users.noreply.github.com>
2023-01-31 13:47:34 -0700

    Update LocationPrompt.swift

--- sun_test/sun_test/LocationPrompt.swift
+++ sun_test/sun_test/LocationPrompt.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+//
+//  ContentView.swift
+//  sun_test
+//
+//  Created by Lena Fleischer on 1/30/23.
+//
+
+import SwiftUI
+
+let backgroundColor = LinearGradient(
+    colors: [Color.pink, Color.orange],
+    startPoint: .top, endPoint: .bottom)
+//var city =  ""
+//var state =  ""
+
+
+struct LocationPrompt: View {
+    @State private var city: String = ""
+    @State private var state: String = ""
+    
+    var body: some View {
+        ZStack{
+            backgroundColor
+            VStack {
+                Text("Welcome")
+                    .font(.title)
+                HStack {
+                    TextField("Enter Your City", text: $city)
+                        .textFieldStyle(.roundedBorder)
+                    TextField("Enter Your State", text: $state)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button(action: get_location(user_city:city, user_state:state), label: {
+                                            Image(systemName: "arrow.right.square")
+                                                .font(.title)
+                                        })
+                }
+            }
+        }
+    }
+}
+
+func get_location(user_city: String, user_state: String) -> String {
+    return user_city + "," + user_state
+}
+
+
+struct LocationPrompt_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationPrompt()
+    }
+}
