dfa669cd7afaca3d9e0e35e983074445b40e612a
mirandahunter <90637050+mirandahunter@users.noreply.github.com>
2023-03-22 10:44:49 -0600

    Update flickr_collection.py

--- data_collection/flickr_collection.py
+++ data_collection/flickr_collection.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+#some of these values change! change tag, text based on type of call you want to make, lat and lon if you make geolocation call
+#don't change api_key, page, page_num, radius
+#num_valid isn't used for anything i believe i was using it as a checker at some point
 api_key = "484f654760142c97321906e5b5325199"
 tag = "sunrise%2CColorado+Springs"
 #text = "sunrise+Colorado+Springs"
@@ -12,19 +15,22 @@ page = "1"
 page_num = int(page)
 num_valid = 0
 
+
+#UNCOMMENT THE CALL YOU WANT TO MAKE, COMMENT THE OTHERS OUT. !!!!IMPORTANT!!!!! COPY THE RESPONSE CALL HERE AND PASTE IT INTO THE RESPONSE CALL ON LINE 64
 """
-#already in file
+#this is the geolocation call, use text "sunrise"
 response = requests.get("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key="+api_key+
 	"&text="+text+"&has_geo=1&lat="+lat+"&lon="+lon+"&radius="+radius+
 	"&radius_units=mi&extras=date_taken&page="+page+"&format=json&nojsoncallback=1").text
 
 """
 """
-#haven't used  yet, might not be as accurate
+#the largest, most results call, use text "sunrise+Denver"
 response = requests.get("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key="+api_key+
 	"&text="+text+"&extras=date_taken&page="+page+"&format=json&nojsoncallback=1").text
 """
 
+#gets photos tagged sunrise and Denver, use tag "sunrise%2CDenver"
 response = requests.get("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key="+api_key+
 	"&tags="+tag+"&tag_mode=all&extras=date_taken&page="+page+"&format=json&nojsoncallback=1").text
 
@@ -41,6 +47,7 @@ date_list = []
 for photo in photo_dict:
 	this_date = photo['datetaken']
 	this_time = this_date[11:]
+	#you can get rid of these "if" checkers if you're checking for sunrise times in the ML part
 	#if(this_time < "10:00:00"):
 	if(this_time < "10:00:00" and photo['owner']!= "33499543@N08"):
 		#print(this_date)
@@ -53,6 +60,7 @@ if(num_pages > 1):
 		page = str(page_num)
 		
 
+		#IMPORTANT!!!!!!!: MAKE SURE THIS MATCHES THE API CALL THAT YOU'RE USING ON LINES 19-31 JUST COPY AND PASTE THE SAME THING
 		response = requests.get("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key="+api_key+
 			"&tags="+tag+"&tag_mode=all&extras=date_taken&page="+page+"&format=json&nojsoncallback=1").text
 
@@ -67,6 +75,7 @@ if(num_pages > 1):
 			this_date = photo['datetaken']
 			this_time = this_date[11:]
 			#print(this_time)
+			#get rid of these if checkers
 			if(this_time < "10:00:00" and photo['owner']!= "33499543@N08"):
 			#if(this_time < "10:00:00"):
 				num_valid += 1
