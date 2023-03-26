2ebcdd87e3e6f5a34beb8f8ae3e1ef0e263c2604
Miranda Hunter <mirandahunter@MacBook-Air-97.local>
2023-03-26 13:26:50 -0600

    fixing rotation issues!

--- sun_test/sun_test/sun_testApp.swift
+++ sun_test/sun_test/sun_testApp.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 @main
+
 struct sun_testApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             AddLocation()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+        
+    static var orientationLock = UIInterfaceOrientationMask.portrait //By default you want all your views to rotate freely
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
